f28d77b765e3d11a939e26f281ab2d88b6c16820
Glenn Jamie <162126044+morkvamora@users.noreply.github.com>
2024-03-04 13:55:13 +0200

    Create codemain.py

---
+++ codemain.py
@@ -0,0 +1,33 @@
+if __name__ == "__main__":  # pragma: no cover
+    if USER_TOKEN:
+        for key, value in fetch_github_info(USER_TOKEN).items():
+            print(f"{key}: {value}")
+    else:
+        raise ValueError("'USER_TOKEN' field cannot be empty.")
+
+  def fetch_jobs(location: str = "mumbai") -> Generator[tuple[str, str], None, None]:
+    soup = BeautifulSoup(requests.get(url + location).content, "html.parser")
+    # This attribute finds out all the specifics listed in a job
+    for job in soup.find_all("div", attrs={"data-tn-component": "organicJob"}):
+        job_title = job.find("a", attrs={"data-tn-element": "jobTitle"}).text.strip()
+        company_name = job.find("span", {"class": "company"}).text.strip()
+        yield job_title, company_name
+
+    if __name__ == "__main__":
+    # Enter a drug name and a zip code
+    drug_name = input("Enter drug name: ").strip()
+    zip_code = input("Enter zip code: ").strip()
+
+    pharmacy_price_list: list | None = fetch_pharmacy_and_price_list(
+        drug_name, zip_code
+    )
+
+    if pharmacy_price_list:
+        print(f"\nSearch results for {drug_name} at location {zip_code}:")
+        for pharmacy_price in pharmacy_price_list:
+            name = pharmacy_price["pharmacy_name"]
+            price = pharmacy_price["price"]
+
+            print(f"Pharmacy: {name} Price: {price}")
+    else:
+        print(f"No results found for {drug_name}")
